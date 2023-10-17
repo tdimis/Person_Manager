8dcb1046a9e7d1f07bdc9d79b396561e68e6eb2c
tdimis <tdimis@gmail.com>
2023-10-17 11:31:56 +0300

    Added fields to the persons table.

--- database/migrations/2023_10_17_070232_create_persons_table.php
+++ database/migrations/2023_10_17_070232_create_persons_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('gender');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('email')->nullable();
+            $table->integer('phone_number')->nullable();
         });
     }
 
