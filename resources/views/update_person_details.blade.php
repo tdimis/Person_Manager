9ebd5b465f94cd5297ff44cc065011d743226457
tdimis <tdimis@gmail.com>
2023-10-19 13:15:06 +0300

    Error messages added about validations. New default select option about gender has been also added.

--- resources/views/update_person_details.blade.php
+++ resources/views/update_person_details.blade.php
@@ -1,23 +1,31 @@
 <h1>Edit Person</h1>
 
+
 <form method="POST" action="{{ route('persons.update', $person) }}">
     @csrf
     @method('PUT')
 
     <div>
         <label for="first_name">First Name:</label>
-        <input type="text" name="first_name" id="first_name" value="{{ $person->first_name }}">
+        <input type="text" name="first_name" id="first_name" value="{{ old('first_name', $person->first_name) }}">
+        @error('first_name')
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
     </div>
 
     <div>
         <label for="last_name">Last Name:</label>
-        <input type="text" name="last_name" id="last_name" value="{{ $person->last_name }}">
+            <input type="text" name="last_name" id="last_name" value="{{ old('last_name', $person->last_name) }}">
+                @error('last_name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
     </div>
 
 
     <div>
         <label for="gender">Gender:</label>
             <select name="gender" id="gender">
+                <option value="">Select Gender</option>
                 <option value="male" {{ $person->gender === 'male' ? 'selected' : '' }}>Male</option>
                 <option value="female" {{ $person->gender === 'female' ? 'selected' : '' }}>Female</option>
             </select>
@@ -32,15 +40,25 @@
     
     <div>
         <label for="email">Email:</label>
-            <input type="email" name="email" id="email" value="{{ $person->email }}">
+            <input type="email" name="email" id="email" value="{{ old('email', $person->email) }}">
+                @if (session('error'))
+                    <span class="alert alert-danger">
+                        {{ session('error') }}
+                    </span>
+                @endif
     </div>
     
     
     <div>
         <label for="phone_number">Phone Number:</label>
-            <input type="tel" name="phone_number" id="phone_number" value="{{ $person->phone_number }}">
+            <input type="tel" name="phone_number" id="phone_number" value="{{ old('phone_number', $person->phone_number) }}">
+                @error('phone_number')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
     </div>
 
     <button type="submit"> Update Person </button>
 
 </form>
+
+
