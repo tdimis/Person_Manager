549fe2ceb320be0bf8f97e9c5d30bdfed70dc0ad
tdimis <tdimis@gmail.com>
2023-10-23 15:08:55 +0300

    Style removed to public app.css. Input borders updated base on validation errors.

--- resources/views/update_person_details.blade.php
+++ resources/views/update_person_details.blade.php
@@ -2,6 +2,8 @@
 <html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     <head>
         <title>Update Person</title>
     </head>
@@ -16,76 +18,50 @@
                         <div class="row g-3 align-items-center">
                             <div class="col-5">
                                 <label for="first_name" class="form-label">First Name:</label>
-                                <input type="text" class="form-control" name="first_name" id="first_name" value="{{ old('first_name', $person->first_name) }}">
+                                <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" id="first_name" value="{{ old('first_name', $person->first_name) }}">
                             </div>
                             <div class="col-5">
                                 <label for="last_name" class="form-label">Last Name:</label>
-                                <input type="text" class="form-control" name="last_name" id="last_name" value="{{ old('last_name', $person->last_name) }}">
+                                <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" id="last_name" value="{{ old('last_name', $person->last_name) }}">
                             </div>
                             <div class="col-5">
                                 <label for="gender" class="form-label">Gender:</label>
                                 <select name="gender" id="gender" class="form-select form-select-sm" aria-label="Small select example">
                                     <option value="">Select Gender</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
+                                    <option value="male" @if(old('gender') === 'male') selected @endif>Male</option>
+                                    <option value="female" @if(old('gender') === 'male') selected @endif>Female</option>
                                 </select>
                             </div>
                             <div class="col-5">
                                 <label for="date_of_birth" class="form-label">Date of Birth:</label>
-                                <input type="date" class="form-control" name="date_of_birth" id="date_of_birth" value="{{ old('date_of_birth', $person->date_of_birth) }}">
+                                <input type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" id="date_of_birth" value="{{ old('date_of_birth', $person->date_of_birth) }}">
                             </div>
                             <div class="col-5">
                                 <label for="email" class="form-label">Email:</label>
-                                <input type="email" class="form-control" name="email" id="email" value="{{ old('email',$person->email) }}">
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" value="{{ old('email',$person->email) }}">
                             </div>
                             <div class="col-5">
                                 <label for="phone_number" class="form-label">Phone Number:</label>
-                                <input type="tel" class="form-control" name="phone_number" id="phone_number" value="{{ old('phone_number', $person->phone_number) }}">
+                                <input type="tel" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" id="phone_number" value="{{ old('phone_number', $person->phone_number) }}">
                             </div>
                         </div>
                         <div class="col-10 text-center">
                             <button class="btn btn-primary" type="submit">Save</button>
+                            <a href="{{ route('persons.store')  }}" class="btn btn-secondary">Cancel</a>
                         </div>
                     </form>
                 </div>
             </div>
+        </div>
+        <div class="error-container">
+            @if($errors->any())
+                @foreach($errors->all() as $error)
+                    <div class="alert alert-danger error-message">
+                        <i class="fas fa-exclamation-triangle"></i>
+                        {{ $error }}
+                    </div>
+                @endforeach
+            @endif
         </div>   
    </body>
 </html>
-
-<style>
-    .error-container {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        padding: 15px;
-        text-align: right;
-    }
-
-    .error-message {
-        display: inline-block;
-        margin-right: 10px;
-        animation: fadeOut 8s forwards;
-    }
-
-    @keyframes fadeOut {
-        0% {
-            opacity: 1;
-        }
-        100% {
-            opacity: 0;
-            display: none;
-        }
-    }
-</style>
-
-<div class="error-container">
-    @if($errors->any())
-        @foreach($errors->all() as $error)
-            <div class="alert alert-danger error-message">
-                <i class="fas fa-exclamation-triangle"></i>
-                    {{ $error }}
-            </div>
-        @endforeach
-    @endif
-</div>
